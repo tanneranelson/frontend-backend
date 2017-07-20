89cd675fc528ab308f0fa5a347bb6c0ff8c379ba
tanneranelson <tanneranelson2000@gmail.com>
2017-07-20 14:07:58 -0600

    Added solution button

--- src/hangman/components/Videogames.js
+++ src/hangman/components/Videogames.js
@@ -6,39 +6,49 @@ import { foods } from './answerList';
 import { AppHeader } from './Header';
 import { Images } from './images';
 
-export class Videogames extends Component {
-  // constructor(props) {
-  //   super(props);
-  //
-  //   this.state = { score: 0, randomWord: {RandomWord} };
-  // }
+export class Movies extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: false,
+    };
+    this.randindex = Math.floor(Math.random() * movies.length)
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      clicked: true,
+    });
+  }
+
   createWord = () => {
     var word = ""
-    var randindex = Math.floor(Math.random() * games.length)
-    for (var i = 0; i < games[randindex].length; i++) {
-      if (games[randindex].charAt(i) == ' ') {
+    for (var i = 0; i < movies[this.randindex].length; i++) {
+      if (movies[this.randindex].charAt(i) == ' ') {
         word += "  "
       } else {
         word += "_ "
       }
     }
-    console.log(word);
+    //console.log(word);
     return (
       <div>
-        <h1 style={{whiteSpace: "pre", fontSize: 80}}>{word}</h1>
-        <h1>{games[randindex]}</h1>
+        <h1 style={{whiteSpace: "pre"}}>{word}</h1>
       </div>
     )
   }
 
-  
   render () {
     return (
-      <div style={{textAlign: "center"}}>
-        <AppHeader /><br />
-        <h2>Video Games</h2>
+      <div>
+        <AppHeader />
         <Images />
+        movies
         {this.createWord()}
+        <button onClick={this.handleClick}>Solution</button>
+        <h1>{this.state.clicked ? movies[this.randindex] : null}</h1>
       </div>
     );
   }
